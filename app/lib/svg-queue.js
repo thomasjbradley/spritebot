ba805273eecc070550948cdfa9ff43162dced087
Thomas J Bradley <hey@thomasjbradley.ca>
2016-12-14 11:45:49 -0500

    Prevent files from being added more than once

--- app/lib/svg-queue.js
+++ app/lib/svg-queue.js
@@ -21,7 +21,17 @@ const run = function (processor, opts, next) {
   run(processor, opts, next);
 };
 
+const alreadyExists = function (path) {
+  let isFound = allFiles.find(function (item) {
+    return item.path == path;
+  });
+
+  return !!isFound;
+};
+
 const add = function (path) {
+  if (alreadyExists(path)) return;
+
   allFiles.push({
     id: allFiles.length,
     path: path,
